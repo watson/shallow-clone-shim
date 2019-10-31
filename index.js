99aaf5c2d53426d0829e837be71a4c9589b5308f
Thomas Watson <w@tson.dk>
2019-10-31 16:04:55 +0100

    feat: add support for Node.js 6

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const getOwnPropertyDescriptors = Object.getOwnPropertyDescriptors || require('get-own-property-descriptors-polyfill')
+
 module.exports = function clone (obj, orig, shim = {}) {
-  const descriptors = Object.getOwnPropertyDescriptors(orig)
+  const descriptors = getOwnPropertyDescriptors(orig)
 
   for (const name of Object.keys(shim)) {
     descriptors[name] = shim[name](descriptors[name])
